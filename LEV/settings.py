78051690c9dbf7962c1105d8feafe64d09d6924e
Lucas <ramirezrodriguezlucas@gmail.com>
2023-08-13 00:53:05 -0400

    changed

--- LEV/settings.py
+++ LEV/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'LEV.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'levDB',
+        'USER': 'root',
+        'PASSWORD': 'Pelusalucas450',
+        'HOST': 'localhost',
+        'PORT': 3306
+    }
 }
 
 # 'ENGINE': 'django.db.backends.mysql',
